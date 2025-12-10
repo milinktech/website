12d08ebe19edb39f5aa8d0cf3df4d7b9b10918d0
Wang, Hao FH <hao.wang1@fraserhealth.ca>
2025-12-09 16:23:06 -0800

    added authenticiation

--- src/FWW.Site.UI/Program.cs
+++ src/FWW.Site.UI/Program.cs
@@ -6,6 +6,23 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Configure HttpClient for API calls
+builder.Services.AddScoped(sp => new HttpClient 
+{ 
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+});
+
+// Configure MSAL Authentication
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    
+    // Use basic Graph scope for login (API scopes added later for backend calls)
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("profile");
+    
+    // Login behavior
+    options.ProviderOptions.LoginMode = "redirect";
+});
 
 await builder.Build().RunAsync();
